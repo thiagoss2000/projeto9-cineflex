46952a2feec3b193612e45174c859d3368c62cbb
thiagoss2000 <thiago.ss675@gmail.com>
2022-03-26 22:18:58 -0300

    tela inicial

---
+++ src/Rota.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+//import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+export default function Rota() {
+
+    const [filmes, setfilmes] = useState({});
+    useEffect(() => {
+        const requisicao = axios.get(
+            `https://mock-api.driven.com.br/api/v5/cineflex/movies`
+        );  
+        requisicao.then((res) => {
+            setfilmes(res.data);
+        });
+    }, []);
+
+    if (filmes.length === undefined){
+        return <p>loading</p>
+    }else{
+        return (
+            <>
+            <div className="subtitulo">
+                <p>Selecione o filme</p>
+            </div>
+            <div className="corpo">
+                {filmes.map((filme) => {
+                    return (
+                        <div key={filme.id}className="filme">
+                            <img src={filme.posterURL} alt={filme.title}></img>
+                        </div>
+                    )
+                })}
+            </div>
+            </>
+        );
+    }
+}
